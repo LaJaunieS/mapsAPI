f98a009fdc1464753d0f3f7d73e85955f4b3b648
slajaunie <stephanlajaunie@yahoo.com>
2017-05-15 14:43:22 -0700

    fix typo, add cursor pointer rule to button

--- js/search.js
+++ js/search.js
@@ -1,31 +1,34 @@
 //console.log("search js");
 
-var visibleOption = document.getElementById('visible-option');
-var nonVisibleOptions = document.getElementById('nonvisible-options');
-var nonVisibleLiItems = nonVisibleOptions.children;	
-var visible = false;
+!function() {
+	var visibleOption = document.getElementById('visible-option');
+	var nonVisibleOptions = document.getElementById('nonvisible-options');
+	var nonVisibleLiItems = nonVisibleOptions.children;	
+	var visible = false;
 
-function showOptions() {
-	if (!visible) {
-		visibleOption.children[0].innerHTML = "Show me Donuts!"
-		nonVisibleOptions.style.display = "block";
-		visible = true;
-	} else {
+	function showOptions() {
+		if (!visible) {
+			visibleOption.children[0].innerHTML = "Show me Donuts!"
+			nonVisibleOptions.style.display = "block";
+			visible = true;
+		} else {
+			nonVisibleOptions.style.display = "none";
+			visible = false;
+		}
+	}
+
+	function selectOption() {
+		visibleOption.children[0].innerHTML = this.innerHTML;
+		visibleOption.children[0].dataset.searchparams = this.dataset.searchparams;
 		nonVisibleOptions.style.display = "none";
 		visible = false;
 	}
-}
 
-function selectOption() {
-	visibleOption.children[0].innerHTML = this.innerHTML;
-	visibleOption.children[0].title = this.title;
-	nonVisibleOptions.style.display = "none";
-	visible = false;
-}
+	visibleOption.addEventListener('click', showOptions);
 
-visibleOption.addEventListener('click', showOptions);
+	for (var i = 0; i < nonVisibleLiItems.length; i++) {
+		var element = nonVisibleLiItems[i];
+		element.onclick = selectOption;	
+	};
 
-for (var i = 0; i < nonVisibleLiItems.length; i++) {
-	var element = nonVisibleLiItems[i];
-	element.onclick = selectOption;	
-};
+}()
